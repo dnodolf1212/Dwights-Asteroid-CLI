95a0862343ee3d4fc59e24ab5e968a23d78a1335
dnodolf1212 <dnodolf@gmail.com>
2020-05-29 19:24:36 -0700

    environment and run set up with console

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,7 @@ require 'bundler/setup'
 Bundler.require(:default, :developement)
 require "dotenv/load"
 
+require_relative './lib/cli.rb'
+require_relative '.lib/asteroid.rb'
+require_relative './lib/api.rb'
+
