37598c34aabd27a56b5bed3a993ea38858e184a3
dnodolf1212 <dnodolf@gmail.com>
2020-05-31 23:20:06 -0700

    cli class methods, api class testing

--- lib/api.rb
+++ lib/api.rb
@@ -1,20 +1,25 @@
 
 class Api
+    todays_date = Time.now.to_s.split(" ")[0]
     puts "api class loaded"
     KEY = ENV["API_KEY"]
-    todays_date = Time.now.to_s.split(" ")[0]
+    
     url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=#{todays_date}&end_date=#{todays_date}&api_key=DEMO_KEY"
     
     BASE_URL =  "https://api.nasa.gov/neo/rest/v1/feed?start_date=#{todays_date}&end_date=#{todays_date}&api_key=FIZam8paaqjNx2IjOKpRwCZymLdMtlrvcDaq5jSX"
     
     def self.get_profile
+        
         response = RestClient.get(BASE_URL)
         data = JSON.parse(response.body)
         data["near_earth_objects"].each do |date, asteroids|
-            #binding.pry
-            puts asteroids[1]["name"]
             
+
+           
+            puts asteroids[1]["name"]
             puts asteroids[1]["nasa_jpl_url"]
+             binding.pry
+            #Asteroids.new(name, id)
         end
              
     end 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,23 @@
 class Cli
 
+        def run
+                welcome
+                Api.get_profile
+                main
+        end
+
+        def welcome
+                puts "Todays Asteroids and Near-Earth-Objects:"
+        end
+
+        def main
+                print_all
+        end 
+
+        def print_all
+                num = 0 
+                Asteroids.all.each {|rock| puts "#{num +=1}.   #{rock.name}"}
+
     
-        puts "Todays Asteroids and Near-Earth-Objects:"
-    
-    
+                
 end 
